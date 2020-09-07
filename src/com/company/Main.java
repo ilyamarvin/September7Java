032c85d6d8bc217ec750b4e3931977d0bd6ef62a
ivc6-10 <asfd@mail.ru>
2020-09-07 14:08:50 +0300

    Full first work of Java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -7,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         int a[] = {1, 3, 4, 5};
         int sum = 0;
-        int i=0;
+        int i=0, t=0;
         for (i=0; i<a.length; i++) {
             sum= sum + a[i];
         }
@@ -29,6 +31,52 @@ public class Main {
         } while (i<a.length);
         System.out.println("sum by do while: " +sum);
 
-	Scanner scan = new Scanner(System.in);
+        for (i=0; i<args.length; i++) {
+            System.out.print(args[i]+ " ");
+        }
+        System.out.println();
+
+        System.out.print(1);
+        for (int j=2; j<11; j++) {
+            System.out.print(" 1/"+j);
+        }
+        System.out.println();
+
+        int[] h = new int[10];
+        System.out.print("Random by math: ");
+        for (i=0; i<h.length; i++) {
+            h[i] = (int)Math.round((Math.random()*30));
+            System.out.print(h[i]+" ");
+        } System.out.println();
+
+        Arrays.sort(h);
+        System.out.print("Sort array: ");
+        for (i=0; i<h.length; i++) {
+            System.out.print(h[i]+" ");
+        } System.out.println();
+
+        int[] l = new int[10];
+        Random rand = new Random();
+        System.out.print("Random by Random: ");
+        for (i=0; i<l.length; i++) {
+            l[i] = rand.nextInt();
+            System.out.print(l[i]+" ");
+        } System.out.println();
+
+
+        Arrays.sort(l);
+        System.out.print("Sort array: ");
+        for (i=0; i<l.length; i++) {
+            System.out.print(l[i]+" ");
+        } System.out.println();
+
+        Scanner scan = new Scanner(System.in);
+        int fact = scan.nextInt();
+        int result = 1;
+        for (i=1; i<=fact; i++){
+            result = result * i;
+        }
+        System.out.print(result);
+
     }
 }
