90a35ce714f86679df33c48bf4ba7e30ac9e2845
ivc6-10 <asfd@mail.ru>
2020-09-07 13:15:50 +0300

    Full third point of work

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,10 +7,28 @@ public class Main {
     public static void main(String[] args) {
         int a[] = {1, 3, 4, 5};
         int sum = 0;
-        for (int i=0; i<a.length; i++) {
+        int i=0;
+        for (i=0; i<a.length; i++) {
             sum= sum + a[i];
         }
         System.out.println("sum by for: " +sum);
+
+        sum = 0;
+        i=0;
+        while (i<a.length) {
+            sum= sum + a[i];
+            i++;
+        }
+        System.out.println("sum by while: " +sum);
+
+        sum = 0;
+        i=0;
+        do {
+            sum = sum + a[i];
+            i++;
+        } while (i<a.length);
+        System.out.println("sum by do while: " +sum);
+
 	Scanner scan = new Scanner(System.in);
     }
 }
